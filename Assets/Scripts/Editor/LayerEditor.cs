18d4a2abef0254913bebf43ce4499c654ccc4366
Esen <sujumayas@gmail.com>
2017-01-21 12:42:56 -0500

    editor and text handler

---
+++ Assets/Scripts/Editor/LayerEditor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+using System.Reflection;
+
+
+[CanEditMultipleObjects()]
+[CustomEditor(typeof(MeshRenderer),true)]
+public class LayerEditor : Editor {
+
+	public override void OnInspectorGUI() {
+
+		GUILayout.Label ("Custom Sorting Order for 2D");
+
+		Renderer rend = target as Renderer;
+
+		rend.sortingOrder = EditorGUILayout.IntField ("Sorting Order", rend.sortingOrder);
+
+		if (GUI.changed) {
+			EditorUtility.SetDirty (target);
+		}
+	}
+}
