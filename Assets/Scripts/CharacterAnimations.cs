24c46cba05444d3a67c616ae90d2e4d9299692be
Darthoul <hessenrod@gmail.com>
2017-01-22 01:36:23 -0500

    Added a Script for the Character Animations

---
+++ Assets/Scripts/CharacterAnimations.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+
+public class CharacterAnimations : MonoBehaviour {
+
+    public RuntimeAnimatorController idle;
+    public RuntimeAnimatorController moving;
+    public RuntimeAnimatorController working;
+}
